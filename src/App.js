7013784e4c2fbae3ac5d644efec4cf907564620c
Mauro Doneda <maurodoneda@macbookpro.home>
2020-03-05 18:18:28 +0000

    finishing styling the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1> Robofriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+    console.log(event.target.value);
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f1"> Robofriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+        <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
 
 export default App;
